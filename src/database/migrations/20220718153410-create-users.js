f791cdd6cb49453176edd98256e73624f29ff018
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-07-18 13:45:34 -0300

    feat: migration

---
+++ src/database/migrations/20220718153410-create-users.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  /**
+   * 
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize').Sequelize} Sequelize 
+   */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      displayName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      }
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ src/database/migrations/20220718161501-create-post-categories.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        }
+      },
+      categoryId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Categories',
+          key: 'id',
+        }
+      }
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
