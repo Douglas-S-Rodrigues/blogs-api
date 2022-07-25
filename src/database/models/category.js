90ec2e1fe777378098978a023a6211720086644d
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-07-25 14:48:42 -0300

    feat: category model

---
+++ src/database/models/category.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require('sequelize');
+
+const attributes = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  name: {
+    type: DataTypes.STRING
+  },
+}
+
+module.exports = (sequelize) => {
+  const category = sequelize.define('Category', attributes, {
+    tableName: 'Categories',
+    // timestamps: false
+  })
+  return category;
+};
